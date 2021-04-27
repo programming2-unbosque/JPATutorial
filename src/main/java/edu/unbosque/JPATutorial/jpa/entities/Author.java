9ac68634afb7b01f7998a32e2413fab1a0b64c90
Fabian Peña <fabiancpl91@gmail.com>
2021-04-26 21:39:42 -0500

    Adding documentation

--- src/main/java/edu/unbosque/JPATutorial/jpa/entities/Author.java
+++ src/main/java/edu/unbosque/JPATutorial/jpa/entities/Author.java
@@ -20,6 +20,8 @@ public class Author {
     @Column(nullable = false)
     private String name;
 
+    // FetchType.EAGER: When we retrieve a Library, we'll also automatically retrieve all of its corresponding Editions
+    // CascadeType.ALL: Propagates all operations from Author to Books
     @OneToMany(mappedBy = "author", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<Book> books = new ArrayList<>();
 
